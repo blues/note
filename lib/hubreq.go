9e204870523ecc17fcfbd4ed86277ef82fdce7c3
Ray Ozzie <ray@ozzie.net>
2019-10-15 09:22:08 -0400

    distinguish tower from gps location

--- lib/hubreq.go
+++ lib/hubreq.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Blues Inc.  All rights reserved.
+// Copyright 2017 Blues Inc.  All rights reserved. 
 // Use of this source code is governed by licenses granted by the
 // copyright holder including that found in the LICENSE file.
 
